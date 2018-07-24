4f860d27786466e78b5d675bb0219adb2c0c82ea
kseniia.semenova <kseniia.semenova91@gmail.com>
2018-07-24 03:30:57 +0300

    added cucumber dependencies created file .feature prepared definitions (not fully)

---
+++ src/test/java/com/epam/ria/cucumber/redirectToLoginStepsDefs.java
@@ -0,0 +1,20 @@
+package com.epam.ria.cucumber;
+
+import cucumber.annotation.en.Given;
+import cucumber.annotation.en.Then;
+import cucumber.annotation.en.When;
+
+public class redirectToLoginStepsDefs {
+  @Given("^Unauthorized user is on Ria Main Page$")
+  public void openRiaMainPage(){
+
+  }
+  @When("^User \"click\" on Sell Car button$")
+  public void clickSellCarButton(){
+
+  }
+  @Then("^Auto Page displayed a Login Form$")
+  public void loginFormDisplays(){
+
+  }
+}
